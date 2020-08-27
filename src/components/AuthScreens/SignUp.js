671145e12ae01468fab85f0db33f0ef35d7721a1
Xuriwork <xuriwork@gmail.com>
2020-08-27 17:27:17 -0400

    updated sign up added password field validations

--- src/components/AuthScreens/SignUp.js
+++ src/components/AuthScreens/SignUp.js
@@ -1,16 +1,32 @@
-import React from 'react';
+/* eslint-disable no-useless-escape */
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { Auth } from 'aws-amplify';
 import { useForm } from 'react-hook-form';
 import notyf from '../../utils/notyf';
 
 const SignUp = ({ history }) => {
+	const [focus, setFocus] = useState(false);
 	const { register, handleSubmit, watch, errors } = useForm();
+	const password = watch('password') || '';
+	const passwordRequirements = {
+		hasLowercaseLetters: password.search(/[a-z]/) > -1,
+		hasUppercaseLetters: password.search(/[A-Z]/) > -1,
+		hasNumbers: password.search(/\d/) > -1,
+		hasSpecialCharacters:
+			password.search(/[\s~`!@#$%\^&*+=\-\[\]\\';,/{}|\\":<>\?()\._]/) > -1,
+		isAtLeast8Characters: password.length >= 8,
+	};
 
-	const handleSignUp = handleSubmit(async (data) => {
+	const handleInputFocus = () => setFocus(true);
+	const handleInputBlur = () => setFocus(false);
+	
+	const handleSignUp = async (data) => {
 		const { email, password } = data;
-		console.log(email);
 
+		const meetsAllRequirements = Object.keys(passwordRequirements).every((key) => passwordRequirements[key]);
+		if (!meetsAllRequirements) return;
+		
 		try {
 			const { user } = await Auth.signUp({
 				username: email,
@@ -33,7 +49,7 @@ const SignUp = ({ history }) => {
 			console.error('Sign up error:', error);
 			notyf.error(error);
 		}
-	});
+	};
 
 	return (
 		<div className='sign-up-component'>
@@ -44,32 +60,141 @@ const SignUp = ({ history }) => {
 						type='email'
 						name='email'
 						placeholder='Enter your email'
-						ref={register({ required: { value: true, message: 'This field is required' } })}
-					/>
-					<span className='form-input-error'>{errors.email && errors.email.message}</span>
-					<label>Password *</label>
-					<input
-						type='password'
-						name='password'
-						placeholder='Enter your password'
 						ref={register({
 							required: { value: true, message: 'This field is required' },
-							minLength: { value: 8, message: 'Password should be at least 8 characters' },
 						})}
 					/>
-					<span className='form-input-error'>{errors.password && errors.password.message}</span>
+					<span className='form-input-error'>
+						{errors.email && errors.email.message}
+					</span>
+					<label>Password *</label>
+					<div className='password-input-container'>
+						<input
+							type='password'
+							name='password'
+							placeholder='Enter your password'
+							onFocus={handleInputFocus}
+							onBlur={handleInputBlur}
+							ref={register({
+								required: { value: true, message: 'This field is required' },
+								pattern: /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*].{8,}$/,
+							})}
+						/>
+						{focus && (
+							<div className='password-requirments'>
+								<h4>Password must meet the following requirements:</h4>
+								<ul>
+									<li>
+										{passwordRequirements.hasLowercaseLetters ? (
+											<span
+												role='img'
+												aria-label='Password contains at least one lowercase letter'
+											>
+												✔️
+											</span>
+										) : (
+											<span
+												role='img'
+												aria-label='Password does not contain any lowercase letters'
+											>
+												❌
+											</span>
+										)}{' '}
+										At least <strong>one lowercase</strong>
+									</li>
+									<li>
+										{passwordRequirements.hasUppercaseLetters ? (
+											<span
+												role='img'
+												aria-label='Password has uppercase letter'
+											>
+												✔️
+											</span>
+										) : (
+											<span
+												role='img'
+												aria-label='Password does not contain any uppercase letters'
+											>
+												❌
+											</span>
+										)}{' '}
+										At least <strong>one uppercase letter</strong>
+									</li>
+									<li>
+										{passwordRequirements.hasNumbers ? (
+											<span role='img' aria-label='Password contains a number'>
+												✔️
+											</span>
+										) : (
+											<span
+												role='img'
+												aria-label='Password does not contain any numbers'
+											>
+												❌
+											</span>
+										)}{' '}
+										At least <strong>one number</strong>
+									</li>
+									<li>
+										{passwordRequirements.hasSpecialCharacters ? (
+											<span
+												role='img'
+												aria-label='Password contains special characters'
+											>
+												✔️
+											</span>
+										) : (
+											<span
+												role='img'
+												aria-label="Password doesn't contain any special characters"
+											>
+												❌
+											</span>
+										)}{' '}
+										At least <strong>one special characters</strong>
+									</li>
+									<li>
+										{passwordRequirements.isAtLeast8Characters ? (
+											<span
+												role='img'
+												aria-label='Password is at least 8 characters'
+											>
+												✔️
+											</span>
+										) : (
+											<span
+												role='img'
+												aria-label="Password isn't at least 8 characters"
+											>
+												❌
+											</span>
+										)}{' '}
+										At least <strong>8 characters</strong>
+									</li>
+								</ul>
+							</div>
+						)}
+					</div>
+					<span className='form-input-error'>
+						{errors.password && errors.password.message}
+					</span>
 					<label>Confirm Password *</label>
 					<input
 						type='password'
 						name='confirm_password'
 						placeholder='Confirm password'
+						onFocus={handleInputFocus}
+						onBlur={handleInputBlur}
 						ref={register({
 							required: { value: true, message: 'This field is required' },
-							validate: (value) => value === watch('password') || 'Passwords do not match'
+							validate: (value) =>
+								value === watch('password') || 'Passwords do not match',
 						})}
 					/>
-					<span className='form-input-error'>{errors.confirm_password && errors.confirm_password.message}</span>
-					<button onClick={handleSignUp} className='form-button'>
+					<span className='form-input-error'>
+						{errors.confirm_password && errors.confirm_password.message}
+					</span>
+					<button onClick={handleSubmit(handleSignUp)} className='form-button'>
 						Sign Up
 					</button>
 					<p>
